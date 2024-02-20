c73b3fe0619c9e1079f3cd814e352d0cc840a71a
Salihk1git <msalihk1@gmail.com>
2024-02-20 12:32:01 +0530

    modified app.py

--- app.py
+++ app.py
@@ -58,16 +58,24 @@ def index():
 def result():
     return render_template('result.html')
 
+
 @app.route('/process_video', methods=['POST'])
 def process_video():
     try:
-        data = request.get_json() if request.is_json else request.form.to_dict()
+        # Check if the request is JSON
+        if request.is_json:
+            data = request.get_json()
+        else:
+            # If not JSON, assume form data
+            data = request.form.to_dict()
+
         user_name = data.get('userName')
         sender_phone_number = data.get('senderPhoneNumber')
 
+        # Validate user name
         if not user_name:
             return jsonify({"status": "error", "message": "Please enter your name"})
-
+       
         cap = cv2.VideoCapture(video_path)
         fps = cap.get(cv2.CAP_PROP_FPS)
 
@@ -124,6 +132,73 @@ def process_video():
 
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)})
+
+# @app.route('/process_video', methods=['POST'])
+# def process_video():
+#     try:
+#         data = request.get_json() if request.is_json else request.form.to_dict()
+#         user_name = data.get('userName')
+#         sender_phone_number = data.get('senderPhoneNumber')
+
+#         if not user_name:
+#             return jsonify({"status": "error", "message": "Please enter your name"})
+
+#         cap = cv2.VideoCapture(video_path)
+#         fps = cap.get(cv2.CAP_PROP_FPS)
+
+#         # Specify the output video path
+#         temp_output_path = 'temp_output.mp4'
+#         out = cv2.VideoWriter(temp_output_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (int(cap.get(3)), int(cap.get(4))))
+
+#         start_time = 3
+#         stop_time = 9
+
+#         possible_prizes = ["10% OFF ON FOOTWEAR", "20%OFFER ON JEANS", "50% OFFER ON INNERWEAR", "75% OFFER ON TOPWEAR", "25% OFF ON TELEVISION", "GIFT CARD", "45% OFFER ON TV"]
+#         random_prize = random.choice(possible_prizes)
+
+#         while True:
+#             ret, frame = cap.read()
+
+#             if not ret:
+#                 print("No more frames to process.")
+#                 break
+
+#             print("Processing frame...")
+
+#             current_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0
+
+#             if start_time <= current_time <= stop_time:
+#                 user_text_combined = f"{user_name}"
+#                 prize_text_combined = f"You've won a {random_prize}"
+
+#                 max_width = 316
+#                 max_height = 75
+
+#                 frame = add_text_to_frame(frame, user_text_combined, (271, 864), max_width, max_height)
+#                 frame = add_text_to_frame(frame, prize_text_combined, (271, 899), max_width, max_height)
+
+#             # Write the modified frame directly to the output video
+#             out.write(frame)
+
+#         cap.release()
+#         out.release()  # Release the VideoWriter
+
+#         print("Number of frames processed.")
+
+#         # Upload the output video file to Cloudinary
+#         cloudinary_response = uploader.upload(temp_output_path, resource_type="video", folder="output_video")
+#         cloudinary_url = cloudinary_response['secure_url']
+
+#         # Remove the temporary video file
+#         os.remove(temp_output_path)
+
+#         # Emit a socket event to inform the client about the generated video
+#         socketio.emit('video_generated', {'video_path': cloudinary_url, 'phone_number': sender_phone_number})
+
+#         return jsonify({"status": "success", "message": "Video generation completed", "video_path": cloudinary_url})
+
+#     except Exception as e:
+#         return jsonify({"status": "error", "message": str(e)})
 @app.route('/static/<filename>')
 def serve_video(filename):
     return send_from_directory('static', filename)
