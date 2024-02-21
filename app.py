0587d982e34cdfb3e38e31372512ee0393df72b1
Salihk1git <msalihk1@gmail.com>
2024-02-21 11:28:17 +0530

    Update file

--- app.py
+++ app.py
@@ -130,6 +130,126 @@
 #     except Exception as e:
 #         return jsonify({"status": "error", "message": str(e)})
 
+# from flask import Flask, render_template, request, jsonify
+# import cloudinary
+# from cloudinary.uploader import upload as cloudinary_upload
+# import cv2
+# import numpy as np
+# import random
+# import os
+# from werkzeug.utils import secure_filename
+
+# app = Flask(__name__)
+
+# cloudinary.config(
+#     cloud_name="dnyqripva",
+#     api_key="648727619819579",
+#     api_secret="8IMHMm-1S4bxZ09DylKyKNS7Zfk"
+# )
+
+# # Cloudinary URL for the input video
+# input_video_url = 'https://res.cloudinary.com/dnyqripva/video/upload/v1708424613/input_video/Copy_of_namith_ks_jkfvd1.mp4'
+
+# def add_text_to_frame(frame, text, position, max_width, max_height, font_scale=1.0):
+#     font = cv2.FONT_HERSHEY_DUPLEX
+#     font_thickness = 2
+#     font_color = (0, 0, 0)
+
+#     # Get the width and height of the text bounding box
+#     (text_width, text_height), baseline = cv2.getTextSize(text, font, font_scale, font_thickness)
+
+#     # Calculate the position to center the text within the specified area
+#     position_x, position_y = position
+#     position_x = int(position_x + (max_width - text_width) / 2)
+#     position_y = int(position_y + (max_height + text_height) / 2)
+
+#     # Add text to the frame with the calculated font size and position
+#     cv2.putText(frame, text, (position_x, position_y), font, font_scale, font_color, font_thickness)
+#     return frame
+
+
+# def allowed_file(filename):
+#     # Accept any file or Cloudinary URL
+#     return True
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/result')
+# def result():
+#     return render_template('result.html')
+
+# @app.route('/process_video', methods=['POST'])
+# def process_video():
+#     try:
+#         # Retrieve user name from the submitted form
+#         user_name = request.form.get('userName')
+
+#         # Validate user name
+#         if not user_name:
+#             return jsonify({"status": "error", "message": "Please enter your name"})
+
+#         # Fetch the input video from Cloudinary
+#         cap = cv2.VideoCapture(input_video_url)
+#         fps = cap.get(cv2.CAP_PROP_FPS)
+
+#         modified_frames = []
+#         possible_prizes = ["car", "bike", "washing machine", "flight ticket", "tour package", "cooker", "refrigerator"]
+#         random_prize = random.choice(possible_prizes)
+
+#         while True:
+#             ret, frame = cap.read()
+
+#             if not ret:
+#                 break
+
+
+#             # Assuming a constant start and stop time for demonstration
+#             start_time = 3
+#             stop_time = 7
+#             current_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0
+
+#             if start_time <= current_time <= stop_time:
+#                 user_text_combined = f"{user_name}"
+#                 prize_text_combined = f"You've won a {random_prize}"
+
+#                 # Assuming max_width and max_height are the dimensions of the specific area
+#                 max_width = 316
+#                 max_height = 75
+
+#                 frame = add_text_to_frame(frame, user_text_combined, (271, 864), max_width, max_height)
+#                 frame = add_text_to_frame(frame, prize_text_combined, (271, 899), max_width, max_height)
+
+#             modified_frames.append(frame)
+
+#         cap.release()
+
+#         if modified_frames:
+#             # Write the modified frames to a temporary video file
+#             temp_output_path = 'temp_output.mp4'
+#             out = cv2.VideoWriter(temp_output_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (modified_frames[0].shape[1], modified_frames[0].shape[0]))
+#             for modified_frame in modified_frames:
+#                 out.write(modified_frame)
+#             out.release()
+
+#             # Upload the temporary video file to Cloudinary
+#             cloudinary_response = cloudinary_upload(temp_output_path, resource_type="video", folder="output_video")
+#             cloudinary_url = cloudinary_response['secure_url']
+
+#             # Remove the temporary video file
+#             os.remove(temp_output_path)
+
+#             return jsonify({"status": "success", "message": "Video generation completed", "video_path": cloudinary_url})
+#         else:
+#             return jsonify({"status": "error", "message": "No frames to process"})
+
+#     except Exception as e:
+#         return jsonify({"status": "error", "message": str(e)})
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 from flask import Flask, render_template, request, jsonify
 import cloudinary
 from cloudinary.uploader import upload as cloudinary_upload
