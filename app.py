bae2745228d67918cdefb7a79f550c1ae486a3b1
Salihk1git <msalihk1@gmail.com>
2024-02-22 10:15:29 +0530

    Update files

--- app.py
+++ app.py
@@ -1,263 +1,15 @@
-# from flask import Flask, render_template, request, jsonify, send_from_directory
-# from flask_socketio import SocketIO
-# from flask_cors import CORS  # Import CORS from the flask_cors extension
-# import cloudinary
-# from cloudinary import config as cloudinary_config, uploader
-# import os
-# import cv2
-# import numpy as np
-# import random
-
-# # Configure Cloudinary
-# cloudinary.config(
-#     cloud_name=os.environ.get("CLOUDINARY_CLOUD_NAME"),
-#     api_key=os.environ.get("CLOUDINARY_API_KEY"),
-#     api_secret=os.environ.get("CLOUDINARY_API_SECRET")
-# )
-
-
-# app = Flask(__name__)
-# CORS(app)  # Add this line to enable CORS
-# socketio = SocketIO(app)
-
-# # Specify video path
-# video_path = 'https://res.cloudinary.com/dnyqripva/video/upload/v1708405239/output_video/displayed.mp4'
-
-# def add_text_to_frame(frame, text, position, max_width, max_height, font_scale=1.0):
-#     font = cv2.FONT_HERSHEY_DUPLEX
-#     font_thickness = 2
-#     font_color = (0, 0, 0)
-
-#     # Get the width and height of the text bounding box
-#     (text_width, text_height), baseline = cv2.getTextSize(text, font, font_scale, font_thickness)
-
-#     # Calculate the position to center the text within the specified area
-#     position_x, position_y = position
-#     position_x = int(position_x + (max_width - text_width) / 2)
-#     position_y = int(position_y + (max_height + text_height) / 2)
-
-#     # Add text to the frame with the calculated font size and position
-#     cv2.putText(frame, text, (position_x, position_y), font, font_scale, font_color, font_thickness)
-#     return frame
-
-
-# def allowed_file(filename):
-#     # Accept any file or Cloudinary URL
-#     return True
-
-# @socketio.on('connect')
-# def handle_connect():
-#     print('Client connected')
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# @app.route('/result')
-# def result():
-#     return render_template('result.html')
-
-
-# @app.route('/process_video', methods=['POST'])
-# def process_video():
-#     try:
-#         # Check if the request is JSON
-#         if request.is_json:
-#             data = request.get_json()
-#             user_name = data.get('userName')
-#         else:
-#             # If not JSON, assume form data
-#             user_name = request.form.get('userName')
-
-#         # Validate user name
-#         if not user_name:
-#             return jsonify({"status": "error", "message": "Please enter your name"})
-
-#         cap = cv2.VideoCapture(video_path)
-#         fps = cap.get(cv2.CAP_PROP_FPS)
-
-#         # Specify the output video path
-#         temp_output_path = 'temp_output.mp4'
-#         out = cv2.VideoWriter(temp_output_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (int(cap.get(3)), int(cap.get(4))))
-
-#         start_time = 3
-#         stop_time = 9
-
-#         possible_prizes = ["10% OFF ON FOOTWEAR", "20%OFFER ON JEANS", "50% OFFER ON INNERWEAR", "75% OFFER ON TOPWEAR", "25% OFF ON TELEVISION", "GIFT CARD", "45% OFFER ON TV"]
-#         random_prize = random.choice(possible_prizes)
-
-#         while True:
-#             ret, frame = cap.read()
-
-#             if not ret:
-#                 print("No more frames to process.")
-#                 break
-
-#             print("Processing frame...")
-
-#             current_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0
-
-#             if start_time <= current_time <= stop_time:
-#                 user_text_combined = f"{user_name}"
-#                 prize_text_combined = f"You've won a {random_prize}"
-
-#                 max_width = 316
-#                 max_height = 75
-
-#                 frame = add_text_to_frame(frame, user_text_combined, (271, 864), max_width, max_height)
-#                 frame = add_text_to_frame(frame, prize_text_combined, (271, 899), max_width, max_height)
-
-#             # Write the modified frame directly to the output video
-#             out.write(frame)
-
-#         cap.release()
-#         out.release()  # Release the VideoWriter
-
-#         print("Number of frames processed.")
-
-#         # Upload the output video file to Cloudinary
-#         cloudinary_response = uploader.upload(temp_output_path, resource_type="video", folder="output_video")
-#         cloudinary_url = cloudinary_response['secure_url']
-
-#         # Remove the temporary video file
-#         os.remove(temp_output_path)
-
-#         # Emit a socket event to inform the client about the generated video
-#         socketio.emit('video_generated', {'video_path': cloudinary_url})
-
-#         return jsonify({"status": "success", "message": "Video generation completed", "video_path": cloudinary_url})
-
-#     except Exception as e:
-#         return jsonify({"status": "error", "message": str(e)})
-
-# from flask import Flask, render_template, request, jsonify
-# import cloudinary
-# from cloudinary.uploader import upload as cloudinary_upload
-# import cv2
-# import numpy as np
-# import random
-# import os
-# from werkzeug.utils import secure_filename
-
-# app = Flask(__name__)
-
-# cloudinary.config(
-#     cloud_name="dnyqripva",
-#     api_key="648727619819579",
-#     api_secret="8IMHMm-1S4bxZ09DylKyKNS7Zfk"
-# )
-
-# # Cloudinary URL for the input video
-# input_video_url = 'https://res.cloudinary.com/dnyqripva/video/upload/v1708424613/input_video/Copy_of_namith_ks_jkfvd1.mp4'
-
-# def add_text_to_frame(frame, text, position, max_width, max_height, font_scale=1.0):
-#     font = cv2.FONT_HERSHEY_DUPLEX
-#     font_thickness = 2
-#     font_color = (0, 0, 0)
-
-#     # Get the width and height of the text bounding box
-#     (text_width, text_height), baseline = cv2.getTextSize(text, font, font_scale, font_thickness)
-
-#     # Calculate the position to center the text within the specified area
-#     position_x, position_y = position
-#     position_x = int(position_x + (max_width - text_width) / 2)
-#     position_y = int(position_y + (max_height + text_height) / 2)
-
-#     # Add text to the frame with the calculated font size and position
-#     cv2.putText(frame, text, (position_x, position_y), font, font_scale, font_color, font_thickness)
-#     return frame
-
-
-# def allowed_file(filename):
-#     # Accept any file or Cloudinary URL
-#     return True
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
-# @app.route('/result')
-# def result():
-#     return render_template('result.html')
-
-# @app.route('/process_video', methods=['POST'])
-# def process_video():
-#     try:
-#         # Retrieve user name from the submitted form
-#         user_name = request.form.get('userName')
-
-#         # Validate user name
-#         if not user_name:
-#             return jsonify({"status": "error", "message": "Please enter your name"})
-
-#         # Fetch the input video from Cloudinary
-#         cap = cv2.VideoCapture(input_video_url)
-#         fps = cap.get(cv2.CAP_PROP_FPS)
-
-#         modified_frames = []
-#         possible_prizes = ["car", "bike", "washing machine", "flight ticket", "tour package", "cooker", "refrigerator"]
-#         random_prize = random.choice(possible_prizes)
-
-#         while True:
-#             ret, frame = cap.read()
-
-#             if not ret:
-#                 break
-
-
-#             # Assuming a constant start and stop time for demonstration
-#             start_time = 3
-#             stop_time = 7
-#             current_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0
-
-#             if start_time <= current_time <= stop_time:
-#                 user_text_combined = f"{user_name}"
-#                 prize_text_combined = f"You've won a {random_prize}"
-
-#                 # Assuming max_width and max_height are the dimensions of the specific area
-#                 max_width = 316
-#                 max_height = 75
-
-#                 frame = add_text_to_frame(frame, user_text_combined, (271, 864), max_width, max_height)
-#                 frame = add_text_to_frame(frame, prize_text_combined, (271, 899), max_width, max_height)
-
-#             modified_frames.append(frame)
-
-#         cap.release()
-
-#         if modified_frames:
-#             # Write the modified frames to a temporary video file
-#             temp_output_path = 'temp_output.mp4'
-#             out = cv2.VideoWriter(temp_output_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (modified_frames[0].shape[1], modified_frames[0].shape[0]))
-#             for modified_frame in modified_frames:
-#                 out.write(modified_frame)
-#             out.release()
-
-#             # Upload the temporary video file to Cloudinary
-#             cloudinary_response = cloudinary_upload(temp_output_path, resource_type="video", folder="output_video")
-#             cloudinary_url = cloudinary_response['secure_url']
-
-#             # Remove the temporary video file
-#             os.remove(temp_output_path)
-
-#             return jsonify({"status": "success", "message": "Video generation completed", "video_path": cloudinary_url})
-#         else:
-#             return jsonify({"status": "error", "message": "No frames to process"})
-
-#     except Exception as e:
-#         return jsonify({"status": "error", "message": str(e)})
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 from flask import Flask, render_template, request, jsonify
 import cloudinary
 from cloudinary.uploader import upload as cloudinary_upload
 import cv2
 import numpy as np
+from io import BytesIO
+from PIL import Image
+import requests
 import random
-import os
-from werkzeug.utils import secure_filename
+
+# Define possible prizes globally
+possible_prizes = ["10% OFF ON FOOTWEAR", "20% OFFER ON JEANS", "50% OFFER ON INNERWEAR", "75% OFFER ON TOPWEAR", "25% OFF ON TELEVISION", "GIFT CARD", "45% OFFER ON TV"]
 
 app = Flask(__name__)
 
@@ -267,41 +19,58 @@ cloudinary.config(
     api_secret="8IMHMm-1S4bxZ09DylKyKNS7Zfk"
 )
 
-# Cloudinary URL for the input video
-input_video_url = 'https://res.cloudinary.com/dnyqripva/video/upload/v1708424613/input_video/Copy_of_namith_ks_jkfvd1.mp4'
+# Cloudinary URL for the input image
+input_image_url = 'https://res.cloudinary.com/dnyqripva/image/upload/v1708519385/input_video/Your_paragraph_text_1_gdhr9u.jpg'
 
-def add_text_to_frame(frame, text, position, max_width, max_height, font_scale=1.0):
+def add_text_to_image(image, text, max_width, max_height, text_position, font_scale=1.0):
+    # Convert the image to an OpenCV format (numpy array)
+    frame = np.array(image)
+
     font = cv2.FONT_HERSHEY_DUPLEX
-    font_thickness = 2
-    font_color = (0, 0, 0)
+    font_thickness = 5
+    font_color = (255, 255, 255)
 
     # Get the width and height of the text bounding box
     (text_width, text_height), baseline = cv2.getTextSize(text, font, font_scale, font_thickness)
 
-    # Calculate the position to center the text within the specified area
-    position_x, position_y = position
-    position_x = int(position_x + (max_width - text_width) / 2)
-    position_y = int(position_y + (max_height + text_height) / 2)
+    # Calculate the position based on the specified text position
+    position_x, position_y = text_position
+    position_x = int(position_x - text_width / 2)
+    position_y = int(position_y + text_height / 2)
 
-    # Add text to the frame with the calculated font size and position
+    # Add text to the image with the calculated font size and position
     cv2.putText(frame, text, (position_x, position_y), font, font_scale, font_color, font_thickness)
-    return frame
 
+    # Convert the modified frame back to PIL Image format
+    result_image = Image.fromarray(frame)
+    return result_image
 
-def allowed_file(filename):
-    # Accept any file or Cloudinary URL
-    return True
+def calculate_font_scale(text, max_width, max_height, font, font_thickness):
+    # Initialize font scale
+    initial_font_scale = 9.0  # You can adjust this initial value as needed
+
+    # Get the width and height of the text bounding box with the initial font scale
+    (text_width, text_height), _ = cv2.getTextSize(text, font, initial_font_scale, font_thickness)
+
+    # Calculate font scales for width and height
+    width_font_scale = max_width / text_width
+    height_font_scale = max_height / text_height
+
+    # Use the minimum of the two font scales to ensure both width and height fit within the specified area
+    font_scale = min(initial_font_scale * width_font_scale, initial_font_scale * height_font_scale)
+
+    return font_scale
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index1.html')
 
 @app.route('/result')
 def result():
-    return render_template('result.html')
+    return render_template('result1.html')
 
-@app.route('/process_video', methods=['POST'])
-def process_video():
+@app.route('/process_image', methods=['POST'])
+def process_image():
     try:
         # Retrieve user name from the submitted form
         user_name = request.form.get('userName')
@@ -310,59 +79,36 @@ def process_video():
         if not user_name:
             return jsonify({"status": "error", "message": "Please enter your name"})
 
-        # Fetch the input video from Cloudinary
-        cap = cv2.VideoCapture(input_video_url)
-        fps = cap.get(cv2.CAP_PROP_FPS)
+        # Fetch the input image from Cloudinary
+        input_image = Image.open(BytesIO(requests.get(input_image_url).content))
 
-        modified_frames = []
-        possible_prizes = ["car", "bike", "washing machine", "flight ticket", "tour package", "cooker", "refrigerator"]
-        random_prize = random.choice(possible_prizes)
+        # Define the parameters for text placement
+        max_width = 316
+        max_height = 75
+        text_position = (810, 864)  # Adjust this position as needed
 
-        while True:
-            ret, frame = cap.read()
+        # Add user's name to the image with adjusted font scale
+        user_font_scale = 1.5  # Adjust this value as needed
+        result_image = add_text_to_image(input_image, user_name, max_width, max_height, text_position, font_scale=user_font_scale)
 
-            if not ret:
-                break
+        # Add random prize text below user's name
+        prize_text = f"You've won a {random.choice(possible_prizes)}"
+        prize_text_position = (text_position[0], text_position[1] + max_height + 20)  # Adjust vertical position as needed
 
+        # Increase the font scale for larger text
+        prize_font_scale = 1.5  # Adjust this value as needed
+        result_image = add_text_to_image(result_image, prize_text, max_width, max_height, prize_text_position, font_scale=prize_font_scale)
 
-            # Assuming a constant start and stop time for demonstration
-            start_time = 3
-            stop_time = 7
-            current_time = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0
+        # Save the modified image to a BytesIO object
+        output_image_bytes = BytesIO()
+        result_image.save(output_image_bytes, format="JPEG")
+        output_image_bytes.seek(0)
 
-            if start_time <= current_time <= stop_time:
-                user_text_combined = f"{user_name}"
-                prize_text_combined = f"You've won a {random_prize}"
+        # Upload the modified image to Cloudinary
+        cloudinary_response = cloudinary_upload(output_image_bytes, resource_type="image", folder="output_images")
+        cloudinary_url = cloudinary_response['secure_url']
 
-                # Assuming max_width and max_height are the dimensions of the specific area
-                max_width = 316
-                max_height = 75
-
-                frame = add_text_to_frame(frame, user_text_combined, (271, 864), max_width, max_height)
-                frame = add_text_to_frame(frame, prize_text_combined, (271, 899), max_width, max_height)
-
-            modified_frames.append(frame)
-
-        cap.release()
-
-        if modified_frames:
-            # Write the modified frames to a temporary video file
-            temp_output_path = 'temp_output.mp4'
-            out = cv2.VideoWriter(temp_output_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (modified_frames[0].shape[1], modified_frames[0].shape[0]))
-            for modified_frame in modified_frames:
-                out.write(modified_frame)
-            out.release()
-
-            # Upload the temporary video file to Cloudinary
-            cloudinary_response = cloudinary_upload(temp_output_path, resource_type="video", folder="output_video")
-            cloudinary_url = cloudinary_response['secure_url']
-
-            # Remove the temporary video file
-            os.remove(temp_output_path)
-
-            return jsonify({"status": "success", "message": "Video generation completed", "video_path": cloudinary_url})
-        else:
-            return jsonify({"status": "error", "message": "No frames to process"})
+        return jsonify({"status": "success", "message": "Image generation completed", "image_path": cloudinary_url})
 
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)})
